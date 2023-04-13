2eab205d42b390a32ddfbd7d37586bf201834cb5
thomas-mescall <122935621+thomas-mescall@users.noreply.github.com>
2023-04-13 12:58:20 -0500

    hope this works

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        String userInput = myScanner;
+
+        System.out.println("Enter a number");
+    }
+}
