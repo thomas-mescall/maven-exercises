a90e49a5345bb343ab144b371c86699d411c60e2
thomas-mescall <122935621+thomas-mescall@users.noreply.github.com>
2023-04-13 14:07:24 -0500

    all done son

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,10 +1,22 @@
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        Scanner myScanner = new Scanner(System.in);
-        String userInput = myScanner;
+        //StringUtils string = new StringUtils();
 
+        Scanner myScanner = new Scanner(System.in);
         System.out.println("Enter a number");
+        String userInput = myScanner.nextLine();
+
+        if (StringUtils.isNumeric(userInput)) {
+            System.out.printf("Your number is: %s", userInput);
+        } else {
+            System.out.printf("You Entered: %s%n", userInput);
+            System.out.println(userInput + " is not a number");
+            System.out.printf("Flipped Case: %s%n", StringUtils.swapCase(userInput));
+            System.out.printf("Reversed: %s%n", StringUtils.reverse(userInput));
+        }
     }
 }
